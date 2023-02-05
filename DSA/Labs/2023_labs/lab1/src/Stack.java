fa2aecff7478199b2f8328b8ae65c571f957256e
IndudiniThennakoon <thennakoonindudini@gmail.com>
2023-02-06 02:46:23 +0530

    feat(insertion) : add 5 characters to the stack

--- DSA/Labs/2023_labs/lab1/src/Stack.java
+++ DSA/Labs/2023_labs/lab1/src/Stack.java
@@ -1,5 +1,3 @@
-package lab1;
-
 public class Stack{
     public char[]stackArray; // declaration of stack char type array
     public int maxSize;
---
+++ DSA/Labs/2023_labs/lab1/src/StackMain.java
@@ -0,0 +1,10 @@
+public class StackMain{
+    public static void main(String[] args) {
+        Stack stack = new Stack (5);
+        stack.push ('S');
+        stack.push ('T');
+        stack.push ('A');
+        stack.push ('C');
+        stack.push ('K');
+    }
+}
