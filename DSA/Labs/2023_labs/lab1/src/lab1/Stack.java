83bec227b0978e9f36bcc86811de2fdd4ed7e89f
IndudiniThennakoon <thennakoonindudini@gmail.com>
2023-02-06 02:09:52 +0530

    feat(constructor) :create parameterized constructor

--- DSA/Labs/2023_labs/lab1/src/lab1/Stack.java
+++ DSA/Labs/2023_labs/lab1/src/lab1/Stack.java
@@ -4,4 +4,13 @@ public class Stack{
     public char[]stackArray; // declaration of stack char type array
     public int maxSize;
     public int top; // top of the stack
+
+    //parameterized construcotor
+    public Stack(int arrSize){
+        maxSize = arrSize; // set array size
+        stackArray = new char[maxSize];
+        top = -1 // no items in the stack
+    }
+
+
 }
