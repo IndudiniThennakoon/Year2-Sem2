c4f31f18fbf57ea7706a1c2a0e0c836439ada4fc
IndudiniThennakoon <thennakoonindudini@gmail.com>
2023-02-06 02:48:55 +0530

    feat(delete) : remove all 5 characters from the stack

--- DSA/Labs/2023_labs/lab1/src/StackMain.java
+++ DSA/Labs/2023_labs/lab1/src/StackMain.java
@@ -1,10 +1,14 @@
 public class StackMain{
     public static void main(String[] args) {
         Stack stack = new Stack (5);
+//        Insert items to the stack
         stack.push ('S');
         stack.push ('T');
         stack.push ('A');
         stack.push ('C');
         stack.push ('K');
+//        remove items form the stack
+        while(!stack.isEmpty())
+            System.out.println(stack.pop());
     }
 }
